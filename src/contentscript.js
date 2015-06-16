cc0a7ee00200b0ea4e523bc3e8d28eb51ed0ca08
晓帆张 <xiaofanzhang@eduroam5-0996.funklan.uni-saarland.de>
2015-06-16 14:01:41 +0200

    all packed

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,31 +1,63 @@
-﻿chrome.runtime.sendMessage({method: "getUsers"}, callBack);
-// add onload event handler for comments ( and new answers)
-// as comments are only loaded when click
-
-function callBack(res){
-    var links = new Set(); 
+﻿var links = new Set(); 
+// load all block users
+chrome.runtime.sendMessage({method: "getUsers"}, function (res){
     for (var x in res)
         links.add(res[x]);
     console.log(links);
-    var comments = document.getElementsByClassName('zm-item-comment');
-    console.log(comments.length);
+    
+    // only answers are loaded now
+    var ans = document.getElementsByClassName('zm-item-answer');
+    blockAnswer(ans);
+});
+
+
+// add onchange event handler for comments ( and new answers)
+// as comments are only loaded when click
+var observerSummary = new MutationSummary({ 
+    callback: handleSummary, 
+    queries: [
+        {element: "div.zm-item-comment"},       // comments
+        {element: "div.zm-item-answer"},        // new answers
+        {element: "div.zm-noti7-content-item"}    // message
+    ]
+});
+
+function handleSummary(summaries){
+    blockComment(summaries[0].added);
+    blockAnswer(summaries[1].added);
+    blockMsg(summaries[2].added);
+}
+
+function blockAnswer(ans){
+    console.log(ans);
+    for (i = 0; i < ans.length; i++) {
+        var author = ans[i].getElementsByClassName('zm-item-answer-author-info')[0];
+        var link = author.getElementsByTagName('a')[0].href;
+        if (links.has(link)) {
+            console.log(link);
+            ans[i].style.display = 'none';
+        }
+    }
+}
+
+function blockComment(comments){
+    console.log(comments);
     for (i = 0; i < comments.length; i++) {
         var link = comments[i].getElementsByTagName('a')[1].href;
-        console.log(link);
         if (links.has(link)) {
+            console.log(link);
             comments[i].style.display = 'none';
         }
     }
 }
 
-function blockComment(links){
-    var ans = document.getElementsByClassName('zm-item-answer');
-    for (i = 0; i < ans.length; i++) {
-        var author = ans[i].getElementsByClassName('zm-item-answer-author-info')[0];
-        var link = author.getElementsByTagName('a')[0].href;
-        console.log(link);
-        if (links.has(link)) {
-            ans[i].style.display = 'none';
+function blockMsg(msgs){
+    console.log(msgs);
+    for (i = 0; i < msgs.length; i++){
+        var link = msgs[i].getElementsByTagName('a')[0].href;
+        if (links.has(link)){
+            console.log(link);
+            msgs[i].style.display = 'none';
         }
     }
 }
